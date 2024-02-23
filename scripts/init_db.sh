7202b79547e03311fde18c162e42bdb9f638eaf4
akmitrich <ak.mitrich@mail.ru>
2024-02-23 22:12:16 +0300

    introduce postres auth database

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -7,7 +7,7 @@ if ! [ -x "$(command -v psql)" ]; then
     exit 1
 fi
 
-if ![ -x "$(command -v sqlx)" ] then
+if ![ -x "$(command -v sqlx)" ]; then
     echo >&2 "Error sqlx is not installed."
     echo >&2 "Use:"
     echo >&2 "  cargo install --version=0.5.7 sqlx-cli --no-default-features --features postgres"
@@ -20,14 +20,19 @@ DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 DB_NAME="${POSTGRES_DB:=auth}"
 DB_PORT="${POSTGRES_PORT:=5432}"
 
-docker run -e POSTGRES_USER=${DB_USER} -e POSTGRES_PASSWORD=${DB_PASSWORD} -e POSTGRES_DB=${DB_NAME} -p "${DB_PORT}":5432 -d postgres postgres -N 1000
+if [ -z "${SKIP_DOCKER} "]; then
+    docker run -e POSTGRES_USER=${DB_USER} -e POSTGRES_PASSWORD=${DB_PASSWORD} -e POSTGRES_DB=${DB_NAME} -p "${DB_PORT}":5432 -d postgres postgres -N 1000
+fi
 
 export PGPASSWORD="${DB_PASSWORD}"
 until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
     >&2 echo "Postgres is still unavailable - sleeping"
     sleep 1
 done
->&2 echo "Postgres is up and running on port ${DB_PORT}!"
+>&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
 
 export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
 sqlx database create
+sqlx migrate run
+
+>&2 echo "Postgres has been migrated, ready to go!"
