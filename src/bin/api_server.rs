d8bc837ec186fd270092af6c328ae43d4b2ef386
akmitrich <ak.mitrich@mail.ru>
2024-02-23 22:44:21 +0300

    add basic configuration

--- src/bin/api_server.rs
+++ src/bin/api_server.rs
@@ -1,6 +1,9 @@
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    println!("Binded at 0.0.0.0:24944");
-    let listener = std::net::TcpListener::bind("0.0.0.0:24944")?;
+    let configuration =
+        play_auth::configuration::get_configuration().expect("Failed to read configuration.");
+    let address = format!("0.0.0.0:{}", configuration.application_port);
+    let listener = std::net::TcpListener::bind(address)?;
+    println!("Binded at {:?}", listener.local_addr()?);
     play_auth::startup::run(listener)?.await
 }
